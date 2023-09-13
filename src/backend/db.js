d03a1a77e4219afcf5eacc8b1e28f0c8b46542f3
Chang Jing Yan <60026139+changjy44@users.noreply.github.com>
2023-09-13 09:46:16 +0800

    Use psql as a service

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,11 +1,17 @@
 const Pool = require('pg').Pool;
+require("dotenv").config();
+
+const host = process.env.PSQL_HOST;
+const password = process.env.PSQL_PASSWORD;
+const user = process.env.PSQL_USER;
+const database = process.env.PSQL_DATABASE;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "123",
-    host: "localhost",
+    user: user,
+    password: password,
+    host: host,
     port: 5432,
-    database: "userservice"
+    database: database
 });
 
 module.exports = pool;

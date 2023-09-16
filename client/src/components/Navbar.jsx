8b4765077ea1764606e702ba67adb423b1aa1dd5
clevon-w <e0726468@u.nus.edu>
2023-09-16 20:14:27 +0800

    Implemented a basic log in frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
             variant={"link"}
             href={"/login-user"}
           >
-            Sign In
+            Log In
           </Button>
           <Button
             as={"a"}
@@ -94,7 +94,7 @@ const Navbar = () => {
               bg: "pink.300",
             }}
           >
-            Sign Up
+            Register
           </Button>
         </Stack>
       </Flex>

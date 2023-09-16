ecaec502e18c46841a25e0694f17d9f58f35416b
clevon-w <e0726468@u.nus.edu>
2023-09-16 13:15:24 +0800

    Implement axios calls for all user routes from the frontend to the backend.

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,7 +4,47 @@ const basePath = "/api";
 
 export const getUserById = async (id) => {
   try {
-    const res = axios.get(`${basePath}/getUsers/${id}`);
+    const res = await axios.get(`${basePath}/getUsers/${id}`);
+    console.log(res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getAllUsers = async () => {
+  try {
+    const res = await axios.get(`${basePath}/getUsers`);
+    console.log(res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const register = async (req) => {
+  try {
+    const res = await axios.post(`${basePath}/register`, req);
+    console.log(res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const login = async (req) => {
+  try {
+    const res = await axios.post(`${basePath}/login`, req);
+    console.log(res);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const logout = async (req) => {
+  try {
+    const res = await axios.post(`${basePath}/logout`, req);
     console.log(res);
     return res;
   } catch (err) {
@@ -14,8 +54,7 @@ export const getUserById = async (id) => {
 
 export const updateUser = async (id, req) => {
   try {
-    const url = `${basePath}/update/${id}`;
-    const res = axios.put(url, req);
+    const res = await axios.put(`${basePath}/update/${id}`, req);
     console.log(res);
     return res;
   } catch (err) {
@@ -25,7 +64,7 @@ export const updateUser = async (id, req) => {
 
 export const deleteUser = async (id) => {
   try {
-    const res = axios.delete(`${basePath}/delete/${id}`);
+    const res = await axios.delete(`${basePath}/delete/${id}`);
     console.log(res);
     return res;
   } catch (err) {

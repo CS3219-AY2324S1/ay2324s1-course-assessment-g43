155423f9973428de52045b786bd770f588a6e84f
ish1506 <ishiuanlee@gmail.com>
2023-09-18 22:37:52 +0800

    Update API documentation

--- src/question-service/src/routes/question-routes.js
+++ src/question-service/src/routes/question-routes.js
@@ -4,21 +4,38 @@ const questionController = require("../controllers/question-controller");
 const router = express.Router();
 
 /**
- * A question
- * @typedef {object} Question
- * @property {integer} questionId - The unique integer ID
- * @property {string} title - The title
- * @property {string} description - The description
+ * A Question Payload for POST and PUT requests
+ * @typedef {object} QuestionPayload
+ * @property {string} title - The question title
+ * @property {string} description - The question description
  * @property {array<string>} category - The array of categories
- * @property {string} complexity - The complexity
+ * @property {string} complexity - The question complexity
+ */
+
+/**
+ * A Question document
+ * @typedef {object} QuestionDocument
+ * @property {string} _id - The document id
+ * @property {integer} questionId - The question id
+ * @property {string} title - The question title
+ * @property {string} description - The question description
+ * @property {array<string>} category - The array of categories
+ * @property {string} complexity - The question complexity
+ * @property {string} __v - The document version
+ */
+
+/**
+ * An error response
+ * @typedef {object} ErrorResponse
+ * @property {string} error - The error message
  */
 
 /**
  * POST /api/questions
  * @summary Creates a new question.
- * @param {Question} request.body.required - question info
- * @return {object} 201 - success response - application/json
- * @return {object} 400 - error response - application/json
+ * @param {QuestionPayload} request.body.required - question info
+ * @return {QuestionDocument} 201 - success response - application/json
+ * @return {ErrorResponse} 400 - error response - application/json
  * @example request - example payload
  * {
  *   "title": "Two Sum",
@@ -26,17 +43,15 @@ const router = express.Router();
  *   "category": ["Array", "Hash Table"],
  *   "complexity": "Easy"
  * }
- * @example response - 200 - example 200 response
+ * @example response - 201 - example 201 response
  * {
- *   "question": {
  *   "_id": "64fefe7816358d7601e35fa0",
  *   "questionId": 6,
  *   "title": "Two Sum",
  *   "description": "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.",
  *   "category": ["Array", "Hash Table"],
- *   "complexity": "Easy"
+ *   "complexity": "Easy",
  *   "__v": 0
- *  }
  * }
  * @example response - 400 - example 400 response
  * {
@@ -49,11 +64,10 @@ router.post("/questions", questionController.createQuestion);
  * GET /api/questions/{id}
  * @summary Gets a question.
  * @param {integer} questionId.path.required - The `questionId`
- * @return {Question} 200 - success response - application/json
- * @return {object} 500 - error response - application/json
+ * @return {QuestionPayload} 200 - success response - application/json
+ * @return {ErrorResponse} 500 - error response - application/json
  * @example response - 200 - example 200 response
  * {
- *   "question": {
  *   "_id": "64fefe7816358d7601e35fa0",
  *   "questionId": 2,
  *   "title": "second question",
@@ -63,7 +77,6 @@ router.post("/questions", questionController.createQuestion);
  *   ],
  *   "complexity": "easy",
  *   "__v": 0
- *  }
  * }
  * @example response - 500 - example 500 response
  * {
@@ -75,39 +88,37 @@ router.get("/questions/:id", questionController.getQuestion);
 /**
  * GET /api/questions
  * @summary Gets all questions.
- * @return {array<Question>} 200 - success response - application/json
- * @return {object} 500 - error response - application/json
+ * @return {array<QuestionPayload>} 200 - success response - application/json
+ * @return {ErrorResponse} 500 - error response - application/json
  * @example response - 200 - example 200 response
- * {
- *   "questions": [
- *     {
- *       "_id": "64fefe5516358d7601e35f9a",
- *       "questionId": 1,
- *       "title": "first question",
- *       "description": "desc1",
- *       "category": [
- *         "cat1",
- *         "cat3",
- *         "cat33"
- *       ],
- *       "complexity": "easy",
- *       "__v": 0
- *     },
- *     {
- *       "_id": "64fefe7816358d7601e35fa0",
- *       "questionId": 2,
- *       "title": "second question",
- *       "description": "desc1",
- *       "category": [
- *         "cat1",
- *         "cat3",
- *         "cat33"
- *       ],
- *       "complexity": "easy",
- *       "__v": 0
- *     }
- *   ]
- * }
+ * [
+ *   {
+ *     "_id": "64fefe5516358d7601e35f9a",
+ *     "questionId": 1,
+ *     "title": "first question",
+ *     "description": "desc1",
+ *     "category": [
+ *       "cat1",
+ *       "cat3",
+ *       "cat33"
+ *     ],
+ *     "complexity": "easy",
+ *     "__v": 0
+ *   },
+ *   {
+ *     "_id": "64fefe7816358d7601e35fa0",
+ *     "questionId": 2,
+ *     "title": "second question",
+ *     "description": "desc1",
+ *     "category": [
+ *       "cat1",
+ *       "cat3",
+ *       "cat33"
+ *     ],
+ *     "complexity": "easy",
+ *     "__v": 0
+ *   }
+ * ]
  * @example response - 500 - example 500 response
  * {
  *  "error": "Error fetching questions"
@@ -119,9 +130,22 @@ router.get("/questions", questionController.getAllQuestions);
  * PUT /api/questions/{id}
  * @summary Updates an existing question.
  * @param {integer} questionId.path.required - The `questionId`
- * @param {Question} request.body.required - question info
- * @return {object} 200 - success reponse - application/json
- * @return {object} 400 - bad request response - application/json
+ * @param {QuestionPayload} request.body.required - The question info
+ * @return {QuestionDocument} 200 - success reponse - application/json
+ * @return {ErrorResponse} 400 - bad request response - application/json
+ * @return {ErrorResponse} 404 - not found response - application/json
+ * @example response - 200 - example 200 response
+ * {
+ *   "_id": "64fefe9c16358d7601e35fa3",
+ *	 "questionId": 3,
+ *	 "title": "New title",
+ *	 "description": "desc1",
+ *	 "category": [
+ *	   "cat1, cat3, cat33"
+ *	 ],
+ *	 "complexity": "easy",
+ *   "__v": 0
+ * }
  * @example response - 400 - example 400 response
  * {
  *  "error": "Error updating question"
@@ -137,12 +161,11 @@ router.put("/questions/:id", questionController.updateQuestion);
  * DELETE /api/questions/{id}
  * @summary Deletes a question.
  * @param {integer} questionId.path.required - The `questionId`
- * @return {object} 200 - success reponse - application/json
- * @return {object} 400 - bad request response - application/json
- * @return {object} 404 - bad request response - application/json
+ * @return {QuestionDocument} 200 - success reponse - application/json
+ * @return {ErrorResponse} 400 - bad request response - application/json
+ * @return {ErrorResponse} 404 - not found response - application/json
  * @example response - 200 - example 200 response
  * {
- *   "question": {
  *   "_id": "64fefe7816358d7601e35fa0",
  *   "questionId": 4,
  *   "title": "fourth question",
@@ -152,7 +175,6 @@ router.put("/questions/:id", questionController.updateQuestion);
  *   ],
  *   "complexity": "easy",
  *   "__v": 0
- *  }
  * }
  * @example response - 400 - example 400 response
  * {

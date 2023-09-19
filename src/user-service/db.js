64a6081a6bac6770feb028a01d5ded806971a68e
Chang Jing Yan <60026139+changjy44@users.noreply.github.com>
2023-09-19 23:50:05 +0800

    Renamed folder to user-service

---
+++ src/user-service/db.js
@@ -0,0 +1,17 @@
+const Pool = require('pg').Pool;
+require("dotenv").config();
+
+const host = process.env.PSQL_HOST;
+const password = process.env.PSQL_PASSWORD;
+const user = process.env.PSQL_USER;
+const database = process.env.PSQL_DATABASE;
+
+const pool = new Pool({
+    user,
+    password,
+    host,
+    port: 5432,
+    database
+});
+
+module.exports = pool;

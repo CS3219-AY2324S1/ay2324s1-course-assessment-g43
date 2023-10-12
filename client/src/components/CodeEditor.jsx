c641a9a2767b99624f8ec36bce60de2eb2102515
Branda <brandaang@gmail.com>
2023-10-12 16:39:05 +0800

    Update editor to take in files of different extensions

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,36 +1,40 @@
-import { Button, HStack, Stack } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { Stack } from "@chakra-ui/react";
+import { useRef, useState, useEffect } from "react";
 import { Editor } from "@monaco-editor/react";
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
 import { MonacoBinding } from "y-monaco";
 
-const files = {
-  "script.py": {
-    name: "script.py",
-    language: "python",
-    value: "print('Hello world')",
-  },
-  "index.html": {
-    name: "index.html",
-    language: "html",
-    value: "<div>hello world </div>",
-  },
-  "script.js": {
-    name: "script.js",
-    language: "javascript",
-    value: "let name = 5",
-  },
-};
-
 export const CodeEditor = () => {
   const editorRef = useRef(null);
 
-  function handleEditorDidMount(editor, monaco) {
+  const options = {
+    autoIndent: "full",
+    contextmenu: true,
+    fontFamily: "monospace",
+    fontSize: 15,
+    lineHeight: 22,
+    hideCursorInOverviewRuler: false,
+    matchBrackets: "always",
+    minimap: {
+      enabled: true,
+    },
+    scrollbar: {
+      horizontalSliderSize: 4,
+      verticalSliderSize: 18,
+    },
+    selectOnLineNumbers: true,
+    roundedSelection: false,
+    readOnly: false,
+    cursorStyle: "line",
+    automaticLayout: true,
+  };
+
+  function handleEditorDidMount(editor) {
     editorRef.current = editor;
     //Init YJS
     const doc = new Y.Doc(); //collection of shared objects
-    //Connect to peers with WebRTC
+    //Connect to peers with WebRTC, diff rooms for diff sessions
     const provider = new WebrtcProvider("test-room", doc);
     const type = doc.getText("monaco");
     //Bind YJS to monaco
@@ -40,38 +44,52 @@ export const CodeEditor = () => {
       new Set([editorRef.current]),
       provider.awareness
     );
-    console.log(provider.awareness);
+    console.log(provider.awareness, binding);
   }
 
-  function getEditorValue() {
-    alert(editorRef.current.getValue());
-  }
+  const [code, setCode] = useState("");
+  const [file, setFile] = useState();
+  const [language, setLanguage] = useState("javascript");
 
-  const [fileName, setFileName] = useState("script.py");
-  const file = files[fileName];
+  const handleFileChange = (event) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
+  useEffect(() => {
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = async (e) => {
+        setCode(e.target.result);
+      };
+      reader.readAsText(file);
+      let newLanguage = "javascript"; //default is javascript
+      const extension = file.name.split(".").pop();
+      // python language name diff from extension name
+      if (extension == "py") {
+        newLanguage = "python";
+      } else if (["cpp", "css", "html"].includes(extension)) {
+        // can add on other languages next time
+        newLanguage = extension;
+      }
+      setLanguage(newLanguage);
+    }
+  }, [file]);
 
   return (
     <Stack align={"center"}>
-      <HStack>
-        <Button onClick={() => setFileName("script.py")}>
-          Switch to script.py
-        </Button>
-        <Button onClick={() => setFileName("index.html")}>
-          Switch to index.html
-        </Button>
-        <Button onClick={() => setFileName("script.js")}>
-          Switch to script.js
-        </Button>
-        <Button onClick={() => getEditorValue()}>Get Editor Value</Button>
-      </HStack>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+      </div>
       <Editor
         height={"50vh"}
         width={"90vw"}
         theme={"vs-dark"}
         onMount={handleEditorDidMount}
-        path={file.name}
-        defaultLanguage={file.language}
-        defaultValue={file.value}
+        language={language}
+        value={code}
+        options={options}
       />
     </Stack>
   );

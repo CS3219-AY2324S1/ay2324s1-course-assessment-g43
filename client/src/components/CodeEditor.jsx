1803dcd7d16adc8fc04029c2667106382d5f4b1a
Branda <brandaang@gmail.com>
2023-10-12 15:48:16 +0800

    Add different language files to editor

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,8 +1,27 @@
-import { useRef } from "react";
+import { Button, HStack, Stack } from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
 import * as Y from "yjs";
 import { WebrtcProvider } from "y-webrtc";
 import { MonacoBinding } from "y-monaco";
+
+const files = {
+  "script.py": {
+    name: "script.py",
+    language: "python",
+    value: "print('Hello world')",
+  },
+  "index.html": {
+    name: "index.html",
+    language: "html",
+    value: "<div>hello world </div>",
+  },
+  "script.js": {
+    name: "script.js",
+    language: "javascript",
+    value: "let name = 5",
+  },
+};
 
 export const CodeEditor = () => {
   const editorRef = useRef(null);
@@ -24,12 +43,36 @@ export const CodeEditor = () => {
     console.log(provider.awareness);
   }
 
+  function getEditorValue() {
+    alert(editorRef.current.getValue());
+  }
+
+  const [fileName, setFileName] = useState("script.py");
+  const file = files[fileName];
+
   return (
-    <Editor
-      height={"50vh"}
-      width={"90vw"}
-      theme={"vs-dark"}
-      onMount={handleEditorDidMount}
-    />
+    <Stack align={"center"}>
+      <HStack>
+        <Button onClick={() => setFileName("script.py")}>
+          Switch to script.py
+        </Button>
+        <Button onClick={() => setFileName("index.html")}>
+          Switch to index.html
+        </Button>
+        <Button onClick={() => setFileName("script.js")}>
+          Switch to script.js
+        </Button>
+        <Button onClick={() => getEditorValue()}>Get Editor Value</Button>
+      </HStack>
+      <Editor
+        height={"50vh"}
+        width={"90vw"}
+        theme={"vs-dark"}
+        onMount={handleEditorDidMount}
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+      />
+    </Stack>
   );
 };

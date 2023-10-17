7b498f0a49783f1358dd0d5f7fba7ceb2a688593
Branda <brandaang@gmail.com>
2023-10-17 18:03:51 +0800

    Add search function

--- client/src/pages/ViewQuestionsUser.jsx
+++ client/src/pages/ViewQuestionsUser.jsx
@@ -6,7 +6,6 @@ import {
   HStack,
   Text,
   Stack,
-  IconButton,
   Input,
   useDisclosure,
   Modal,
@@ -15,12 +14,15 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
+  ModalFooter,
   Tag,
   TagLabel,
   Badge,
   Divider,
   AbsoluteCenter,
   Box,
+  InputGroup,
+  InputLeftElement,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { observer } from "mobx-react";
@@ -49,19 +51,23 @@ export const ViewQuestionsUser = observer(() => {
   return (
     <PageContainer w={"100%"}>
       <Stack spacing={4} w={"100%"}>
-        <HStack justifyContent={"space-between"}>
+        <Stack justifyContent={"space-between"} direction={["column", "row"]}>
           <Text fontSize="40px" align={"start"}>
             Questions
           </Text>
           <HStack>
-            <Input variant="outline" placeholder="Search" width="400px" />
-            <IconButton
-              aria-label="Search database"
-              icon={<SearchIcon />}
-              variant={"outline"}
-            />
+            <InputGroup maxW={"400px"}>
+              <InputLeftElement pointerEvents="none">
+                <SearchIcon />
+              </InputLeftElement>
+              <Input
+                variant="outline"
+                placeholder="Search"
+                onChange={(e) => store.setSearchQuery(e.target.value)}
+              />
+            </InputGroup>
           </HStack>
-        </HStack>
+        </Stack>
         <Flex justifyContent={"space-between"} px={6}>
           <HStack>
             <Text fontWeight="bold">ID</Text>
@@ -70,25 +76,31 @@ export const ViewQuestionsUser = observer(() => {
           <Text fontWeight="bold">Actions</Text>
         </Flex>
         {!!state.questions ? (
-          state.questions.map((question, index) => {
-            return (
-              <Card key={index}>
-                <CardBody>
-                  <Flex justifyContent={"space-between"}>
-                    <HStack>
-                      <Text>{question.questionId}.</Text>
-                      <Text textOverflow={"ellipsis"} maxW={"inherit"}>
-                        {question.title}
-                      </Text>
-                    </HStack>
-                    <Button onClick={() => handleOpenModal(question)}>
-                      View Details
-                    </Button>
-                  </Flex>
-                </CardBody>
-              </Card>
-            );
-          })
+          state.questions
+            .filter((question) =>
+              question.title
+                .toLowerCase()
+                .includes(state.searchQuery.toLowerCase())
+            )
+            .map((question, index) => {
+              return (
+                <Card key={index}>
+                  <CardBody>
+                    <Flex justifyContent={"space-between"}>
+                      <HStack>
+                        <Text>{question.questionId}.</Text>
+                        <Text textOverflow={"ellipsis"} maxW={"inherit"}>
+                          {question.title}
+                        </Text>
+                      </HStack>
+                      <Button onClick={() => handleOpenModal(question)}>
+                        View Details
+                      </Button>
+                    </Flex>
+                  </CardBody>
+                </Card>
+              );
+            })
         ) : (
           <Card>
             <CardBody>
@@ -134,10 +146,16 @@ export const ViewQuestionsUser = observer(() => {
                       Task Discription
                     </AbsoluteCenter>
                   </Box>
-                  <Text paddingTop={"3"}>
-                    {state.selectedQuestion.description}
-                  </Text>
+                  {viewQuestionsStore.state.selectedQuestion.description &&
+                    viewQuestionsStore.state.selectedQuestion.description
+                      .split("\n")
+                      .map((p) => (
+                        <Text py={1} key="">
+                          {p}
+                        </Text>
+                      ))}
                 </ModalBody>
+                <ModalFooter />
               </ModalContent>
             </Modal>
           </>

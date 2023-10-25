93775b1e68ca32522dc6dda8e8bc2ee40f6347d5
clevon-w <e0726468@u.nus.edu>
2023-10-25 20:06:10 +0800

    [Assignment-4]testing to see some stuff

--- src/question-service/index.js
+++ src/question-service/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config({ path: `.env.${process.env.PEERPREP_ENV}` });
-
+console.log(process.env);
 const cors = require("cors");
 const express = require("express");
 const expressJSDocSwagger = require("express-jsdoc-swagger");
@@ -50,5 +50,7 @@ mongoose.connect(databaseUrl, {
 app.use("/api", require("./src/routes/question-routes"));
 
 app.listen(port, () => {
+  console.log("testing to see if i can see anything");
+  console.log(process.env);
   console.log(`Question service listening on port ${port}`);
 });

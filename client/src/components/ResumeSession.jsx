c734bc037b731733b2ac7af2b58ad7a90c5085d5
Branda <brandaang@gmail.com>
2023-10-26 13:34:45 +0800

    set modal to open on default

--- client/src/components/ResumeSession.jsx
+++ client/src/components/ResumeSession.jsx
@@ -11,43 +11,36 @@ import {
 } from "@chakra-ui/react";
 
 import { observer } from "mobx-react";
+import { useEffect } from "react";
 
 const ResumeSession = observer(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
+  useEffect(() => {
+    onOpen();
+  }, [onOpen]);
+
   return (
-    <>
-      <Button onClick={onOpen}>Open Modal</Button>
+    <Modal
+      closeOnOverlayClick={false}
+      isOpen={isOpen}
+      closeOnEsc={false}
+      isCentered
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Resume session</ModalHeader>
+        <ModalBody pb={6}>
+          It seems that you have an active session, please resume the session.
+        </ModalBody>
 
-      <Modal
-        closeOnOverlayClick={false}
-        isOpen={isOpen}
-        closeOnEsc={false}
-        isCentered
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Resume session</ModalHeader>
-          <ModalBody pb={6}>
-            It seems that you have an active session. Do you want to resume the
-            session?
-          </ModalBody>
-
-          <ModalFooter>
-            <Tooltip hasArrow label="Leave Session">
-              <Button colorScheme="red" variant="outline" onClick={onClose}>
-                Leave
-              </Button>
-            </Tooltip>
-            <Tooltip hasArrow label="Resume Session">
-              <Button colorScheme="green" onClick={onClose}>
-                Resume
-              </Button>
-            </Tooltip>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
+        <ModalFooter>
+          <Button colorScheme="green" onClick={onClose}>
+            Resume
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   );
 });
 

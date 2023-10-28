24ec97ce07b0bf9f337e8a29592e4314c6a4c72d
Chang Jing Yan <60026139+changjy44@users.noreply.github.com>
2023-10-28 16:25:14 +0800

    Initialise matching service

--- src/matching-service/index.js
+++ src/matching-service/index.js
@@ -1,171 +1,120 @@
-import { connect } from "amqplib";
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
+// import { connect } from "amqplib";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import express from "express";
+// import http from "http";
+// import { Server } from "socket.io";
+// import { EASY_QUEUE, MEDIUM_QUEUE, HARD_QUEUE, TIMEOUT_MS } from "./constants.js";
+// import { getQueue, listenToQueue } from "./matching.js";
+
+const { connect } = require("amqplib");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const { EASY_QUEUE, MEDIUM_QUEUE, HARD_QUEUE, TIMEOUT_MS } = require("./constants.js");
+const { getQueue, listenToQueue, listenToReplies } = require("./matching.js");
 
 dotenv.config();
 
 const app = express();
 const port = 5001;
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+const init = async () => {
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/hello", (req, res) => {
-  res.send("Hello world");
-});
-
-const ampqHost = process.env.AMPQ_HOST;
-
-const connection = await connect(ampqHost);
-
-const channel = await connection.createChannel();
-
-const EASY_QUEUE = "easy-queue";
-const MEDIUM_QUEUE = "medium-queue";
-const HARD_QUEUE = "hard-queue";
-
-const TIMEOUT_MS = 35 * 1000;
-
-await channel.assertQueue(EASY_QUEUE, { durable: false, messageTtl: 30000 });
-await channel.assertQueue(MEDIUM_QUEUE, { durable: false, messageTtl: 30000 });
-await channel.assertQueue(HARD_QUEUE, { durable: false, messageTtl: 30000 });
-
-const getQueue = (complexity) => {
-  switch (complexity) {
-    case "Easy":
-      return EASY_QUEUE;
-    case "Medium":
-      return MEDIUM_QUEUE;
-    case "Hard":
-      return HARD_QUEUE;
-    default:
-      return null;
-  }
-};
-
-io.on("connection", (socket) => {
-  console.log(`Connection opened: ${socket.id}`);
-  const timeoutId = setTimeout(() => {
-    socket.disconnect(true);
-  }, TIMEOUT_MS);
-
-  socket.on("match-request", async (message) => {
-    const parsedMessage = JSON.parse(message);
-    const { uid, complexity } = parsedMessage;
-    if (!uid || !complexity) {
-      console.log("Invalid request");
-      console.log(`uid: ${uid}`);
-      console.log(`complexity: ${complexity}`);
-      io.to(socket.id).emit("match-failure", "Missing arguments");
-      return;
-    }
-
-    console.log(`match-request: (${uid}, ${complexity})`);
-
-    const queueName = getQueue(complexity);
-    if (!queueName) {
-      io.to(socket.id).emit("match-failure", "Invalid arguments");
-      return;
-    }
-
-    // polls the queue
-    const dequeuedMessage = await channel.get(queueName);
-
-    // if there is no users in the queue, add the user to the queue
-    if (!dequeuedMessage) {
-      socket.emit("fetch-question", complexity, async (question) => {
-        if (!question) {
-          io.to(socket.id).emit("match-failure", "No question found");
-          return;
-        }
-
-        const questionString = JSON.stringify(question);
-
-        const message = {
-          uid,
-          questionString,
-          socketId: socket.id,
-        };
-
-        console.log("Added to Message Queue:");
-        console.log(message);
-
-        channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)));
-      });
-    } else {
-      // else pair the users up
-      const firstRequest = JSON.parse(dequeuedMessage.content.toString());
-      console.log("MATCHED:");
-
-      const question = JSON.parse(firstRequest.questionString);
-
-      const timestamp = Math.floor(Date.now() / 1000);
-      const roomId = `${timestamp}-${firstRequest.uid}-${uid}`;
-
-      const sessionDetails = {
-        roomId: roomId,
-        firstUserId: firstRequest.uid,
-        secondUserId: uid,
-        ...question,
-      };
-
-      socket.emit("create-session", sessionDetails, async (session) => {
-        if (!session) {
-          io.to(socket.id).emit("match-failure", "Failed to create session! User is possibly in another room");
-          io.to(firstRequest.socketId).emit("match-failure", "Failed to create session! User is possibly in another room");
-          return;
-        }
-
-        console.log(session);
-
-        io.to(firstRequest.socketId).emit("match-success", session);
-        io.to(socket.id).emit("match-success", session);
-      });
-    }
+  const server = http.createServer(app);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+  
+  app.use(cors());
+  app.use(express.json());
+  
+  app.get("/api/hello", (req, res) => {
+    res.send("Hello world");
+  });
+  
+  const ampqHost = process.env.AMPQ_HOST;
+  
+  const connection = await connect(ampqHost);
+  
+  const channel = await connection.createChannel();
+  
+  await channel.assertQueue(EASY_QUEUE, { durable: false, messageTtl: 30000 });
+  await channel.assertQueue(MEDIUM_QUEUE, { durable: false, messageTtl: 30000 });
+  await channel.assertQueue(HARD_QUEUE, { durable: false, messageTtl: 30000 });
+  
+  io.on("connection", (socket) => {
+    console.log(`Connection opened: ${socket.id}`);
+    const timeoutId = setTimeout(() => {
+      socket.disconnect(true);
+    }, TIMEOUT_MS);
+  
+    socket.on("match-request", async (message) => {
+      const parsedMessage = JSON.parse(message);
+      const { uid, complexity } = parsedMessage;
+      if (!uid || !complexity) {
+        io.to(socket.id).emit("match-failure", "Missing arguments");
+        return;
+      }
+  
+      const queueName = getQueue(complexity);
+  
+      if (!queueName) {
+        io.to(socket.id).emit("match-failure", "Invalid arguments");
+        return;
+      }
+  
+      const messageMatchRequest = {
+        isCancelRequest: false,
+        userId: uid,
+        socketId: socket.id,
+      }
+  
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(messageMatchRequest)));
+    });
+  
+    socket.on("cancel-request", async (message) => {
+      const parsedMessage = JSON.parse(message);
+      const { uid, complexity } = parsedMessage;
+      if (!uid || !complexity) {
+        // TODO: Emit different event for cancel failure (+ handle event in FE)
+        io.to(socket.id).emit("match-failure", "Missing arguments");
+        return;
+      }
+  
+      const queueName = getQueue(complexity);
+  
+      if (!queueName) {
+        io.to(socket.id).emit("match-failure", "Invalid arguments");
+        return;
+      }
+  
+      const messageCancelRequest = {
+        isCancelRequest: true,
+        userId: uid,
+        socketId: socket.id,
+      }
+  
+      channel.sendToQueue(queueName, Buffer.from(JSON.stringify(messageCancelRequest)));
+    });
+  
+    socket.on("disconnect", () => {
+      clearTimeout(timeoutId);
+      console.log("User has left");
+    });
   });
 
-  socket.on("cancel-request", async (message) => {
-    const parsedMessage = JSON.parse(message);
-    const { uid, complexity } = parsedMessage;
-    if (!uid || !complexity) {
-      console.log("Invalid request");
-      console.log(`uid: ${uid}`);
-      console.log(`complexity: ${complexity}`);
-      // TODO: Emit different event for cancel failure (+ handle event in FE)
-      io.to(socket.id).emit("match-failure", "Missing arguments");
-      return;
-    }
+  listenToQueue(channel);
+  listenToReplies(channel, io);
 
-    console.log(`cancel request: (${uid}, ${complexity})`);
-
-    const queueName = getQueue(complexity);
-    if (!queueName) {
-      io.to(socket.id).emit("match-failure", "Invalid arguments");
-      return;
-    }
-
-    // Removes the current message in the queue.
-    await channel.get(queueName);
-
-    socket.disconnect(true);
+  server.listen(port, () => {
+    console.log(`listening on port ${port}`);
   });
+}
 
-  socket.on("disconnect", () => {
-    clearTimeout(timeoutId);
-    console.log("User has left");
-  });
-});
+init();
 
-server.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
---
+++ src/matching-service/matching.js
@@ -0,0 +1,149 @@
+// import { EASY_QUEUE, HARD_QUEUE, MEDIUM_QUEUE, MATCH_REPLY_QUEUE, QUESTION_COMPLEXITY_LIST, TIMEOUT_MS } from "./constants.js";
+const { EASY_QUEUE, HARD_QUEUE, MEDIUM_QUEUE, MATCH_REPLY_QUEUE, QUESTION_COMPLEXITY_LIST, TIMEOUT_MS } = require("./constants.js");
+
+const getQueue = (complexity) => {
+  switch (complexity) {
+    case QUESTION_COMPLEXITY_LIST[0]:
+      return EASY_QUEUE;
+    case QUESTION_COMPLEXITY_LIST[1]:
+      return MEDIUM_QUEUE;
+    case QUESTION_COMPLEXITY_LIST[2]:
+      return HARD_QUEUE;
+    default:
+      return null;
+  }
+};
+
+const handleCancelRequest = async (channel, index, socketId, userId, pendingSocketRequests, pendingUserIds, pendingTimeouts) => {
+  await channel.assertQueue(MATCH_REPLY_QUEUE, { durable: false, messageTtl: 30000 });
+
+  if (!pendingSocketRequests[index]) {
+    return;
+  }
+  
+  if (pendingSocketRequests[index] === socketId) {
+    clearTimeout(pendingTimeouts[index]);
+    pendingSocketRequests[index] = null;
+
+    const matchCancelMesssage = {
+      isMatchCancel: true,
+      socketId,
+    }
+    channel.sendToQueue(MATCH_REPLY_QUEUE, Buffer.from(JSON.stringify(matchCancelMesssage)));
+  }
+}
+
+const handleSaveMatchRequest = async (channel, index, socketId, userId, pendingSocketRequests, pendingUserIds, pendingTimeouts) => {
+  const messageTimeout = setTimeout(() => {
+    pendingSocketRequests[index] = null;
+  }, TIMEOUT_MS);
+  
+  pendingSocketRequests[index] = socketId;
+  pendingUserIds[index] = userId;
+  pendingTimeouts[index] = messageTimeout;
+}
+
+const handleSuccessMatchRequest = async (channel, index, socketId, userId, pendingSocketRequests, pendingUserIds, pendingTimeouts) => {
+  await channel.assertQueue(MATCH_REPLY_QUEUE, { durable: false, messageTtl: 30000 });
+
+  clearTimeout(pendingTimeouts[index]);
+
+  const firstUserSocketId = pendingSocketRequests[index];
+  const firstUserId = pendingUserIds[index];
+
+  const secondUserSocketId = socketId;
+  const secondUserId = userId;
+
+  pendingSocketRequests[index] = null;
+  pendingUserIds[index] = null;
+
+  const matchSuccessMessage = {
+    isMatchCancel: false,
+    firstUserSocketId,
+    secondUserSocketId,
+    firstUserId, 
+    secondUserId,
+    complexity: QUESTION_COMPLEXITY_LIST[index]
+  };
+
+  channel.sendToQueue(MATCH_REPLY_QUEUE, Buffer.from(JSON.stringify(matchSuccessMessage)));
+}
+
+
+exports.listenToQueue = async (channel) => {
+  await channel.assertQueue(EASY_QUEUE, { durable: false, messageTtl: 30000 });
+  await channel.assertQueue(MEDIUM_QUEUE, { durable: false, messageTtl: 30000 });
+  await channel.assertQueue(HARD_QUEUE, { durable: false, messageTtl: 30000 });
+
+  const queueNames = [EASY_QUEUE, MEDIUM_QUEUE, HARD_QUEUE];
+  const pendingSocketRequests = [null, null, null];
+  const pendingUserIds= [null, null, null];
+  const pendingTimeouts = [null, null, null];
+
+  for (let index = 0; index < queueNames.length; index++) {
+    const queueName = queueNames[index];
+
+    channel.consume(queueName, (message) => {
+      if (!message) {
+        return;
+      }
+      const { isCancelRequest, userId, socketId } = JSON.parse(message.content.toString());
+
+      if (isCancelRequest) {
+        handleCancelRequest(channel, index, socketId, userId, pendingSocketRequests, pendingUserIds, pendingTimeouts);
+      } else if (!messageRequests[index]) {
+        handleSaveMatchRequest(channel, index, socketId, userId, pendingSocketRequests, pendingUserIds, pendingTimeouts);
+      } else {
+        handleSuccessMatchRequest(channel, index, socketId, userId, pendingSocketRequests, pendingUserIds, pendingTimeouts);
+      }
+    });
+    
+  }
+}
+
+exports.listenToReplies = async (channel, io) => {
+  await channel.assertQueue(MATCH_REPLY_QUEUE, { durable: false, messageTtl: 30000 });
+
+  channel.consume(MATCH_REPLY_QUEUE, (message) => {
+    const parsedReply = JSON.parse(message.content.toString());
+
+    if (parsedReply.isMatchCancel) {
+      io.to(socket.id).emit("match-cancelled", "Match cancelled");
+      return;
+    }
+
+    const firstUserSocketId = parsedReply.firstUserSocketId;
+    const firstUserId = parsedReply.firstUserId;
+
+    const secondUserSocketId = parsedReply.secondUserSocketId;
+    const secondUserId = parsedReply.secondUserId;
+
+    const complexity = parsedReply.complexity;
+
+    const timestamp = Math.floor(Date.now() / 1000);
+    const roomId = `${timestamp}-${firstUserId}-${secondUserId}`;
+
+    const sessionCreationRequest = {
+      roomId,
+      firstUserId,
+      secondUserId,
+      complexity,
+    };
+
+    io.to(firstUserSocketId).emit("create-session", sessionCreationRequest, async (session) => {
+      if (!session) {
+        io.to(firstUserSocketId).emit("match-failure", "Failed to create session! User is possibly in another room");
+        io.to(secondUserSocketId).emit("match-failure", "Failed to create session! User is possibly in another room");
+        return;
+      }
+
+      io.to(firstRequest.socketId).emit("match-success", session);
+      io.to(socket.id).emit("match-success", session);
+    });
+  });
+}
+
+exports.getQueue = getQueue;
+
+
+

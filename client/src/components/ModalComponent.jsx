bd50b491b9f4514867e2889fa9dc6844851c20b9
Branda <brandaang@gmail.com>
2023-11-13 21:53:33 +0800

    change modal size to 2xl

--- client/src/components/ModalComponent.jsx
+++ client/src/components/ModalComponent.jsx
@@ -21,7 +21,7 @@ const ModalComponent = observer(() => {
       closeOnOverlayClick={modalComponentStore.isClosable}
       isCentered
       scrollBehavior="inside"
-      size={"3xl"}
+      size={"2xl"}
     >
       <ModalOverlay />
       <form onSubmit={modalComponentStore.onSubmit}>
